d6eef36829fcc64d1a4ff433f39a1f414736e98f
Facundo Maldonado <facumaldonado319@gmail.com>
2017-11-22 19:28:29 -0300

    REBASING

--- Alquileres/Inmobiliaria/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from Inmobiliaria.views import *
-
-urlpatterns = [
-    url(r'^$', index),
-    url(r'hosts/', hosts),
-    url(r'estate/(?P<id_estate>\d+)/reserve', index_reserve, name="estate_reserve")
-]
